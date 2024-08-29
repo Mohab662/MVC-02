9565a26e17c5a99dd20eab0b0a0c2683e8adea10
Mohab662 <mohabmbelkan@gmail.com>
2024-08-29 16:25:14 +0300

    Add project files.

---
+++ MVC-02/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_02.Controllers
+{
+    public class MovieController : Controller
+    {
+        public void GetMovie(int id) 
+        {
+        
+        }
+        public string Index()
+        {
+            return "Index";
+        }
+    }
+}
